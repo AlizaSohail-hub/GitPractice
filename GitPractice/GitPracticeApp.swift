5ca7c5999d2fcfc5e6c6bff9511b144fbd060d39
aliza <izaosha@gmail.com>
2026-06-21 15:43:54 -0500

    Initial Commit

---
+++ GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by Aliza Sohail on 6/21/26.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
